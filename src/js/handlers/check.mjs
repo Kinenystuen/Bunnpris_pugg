246d070f6ad1c6ab1625fb8722ad5f9cc6a6318d
Kinenystuen <kine_on@hotmail.com>
2024-05-29 23:59:41 +0200

    nextBtn remove

--- src/js/handlers/check.mjs
+++ src/js/handlers/check.mjs
@@ -6,15 +6,17 @@ export function setCheckForm(product) {
     const inputNumber = parseInt(inputNumberElement.value);
     const correctNumber = product.EAN;
     const correctNumberBox = document.getElementById("correctNumberBox");
+    const nextBtn = document.getElementById("nextBtn");
 
     if (inputNumber === correctNumber) {
       inputNumberElement.classList.remove('is-invalid');
         inputNumberElement.classList.add('is-valid');
         correctNumberBox.style.display = "none";
+        nextBtn.style.display = "none";
         
         setTimeout(function() {
             displayProducts();
-        }, 1000);
+        }, 500);
   } else {
       inputNumberElement.classList.remove('is-valid');
       inputNumberElement.classList.add('is-invalid');
