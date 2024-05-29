9b91d065a831e76d321e9dd7053858844e6b4a82
Kinenystuen <kine_on@hotmail.com>
2024-05-29 22:48:23 +0200

    check answer

---
+++ src/js/handlers/check.mjs
@@ -0,0 +1,28 @@
+import { displayProducts } from "../display/productDisplay.mjs";
+
+export function setCheckForm(product) {
+
+    const inputNumberElement = document.getElementById('productNumber');
+    const inputNumber = parseInt(inputNumberElement.value);
+    const correctNumber = product.EAN;
+    const correctNumberBox = document.getElementById("correctNumberBox");
+    const nextBtn = document.getElementById("nextBtn");
+
+    if (inputNumber === correctNumber) {
+      inputNumberElement.classList.remove('is-invalid');
+      inputNumberElement.classList.add('is-valid');
+      correctNumberBox.style.display = "none";
+      nextBtn.style.display = "block";
+      nextBtn.addEventListener("click", function() {
+        console.log("hello")
+        displayProducts();
+      })
+  } else {
+      inputNumberElement.classList.remove('is-valid');
+      inputNumberElement.classList.add('is-invalid');
+      correctNumberBox.style.display = "block";
+
+  }
+
+    console.log(product.EAN)
+}
