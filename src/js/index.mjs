e2004e48875cc02d5fac399e3499e721c3fb9216
Kinenystuen <kine_on@hotmail.com>
2024-05-29 22:47:04 +0200

    mjs boss

---
+++ src/js/index.mjs
@@ -0,0 +1,7 @@
+import * as display from "./display/index.mjs";
+import * as handlers from "./handlers/index.mjs";
+
+const currentUrl = window.location.href;
+
+display.displayProducts();
+// handlers.setCheckForm();
