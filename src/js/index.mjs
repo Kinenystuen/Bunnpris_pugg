4112e5a0e8989af8e99cc68c17b6b303b604a217
Kinenystuen <kine_on@hotmail.com>
2024-06-04 13:33:32 +0200

    added brodvarer url

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -6,6 +6,6 @@ const currentUrl = window.location.href;
 if (currentUrl.includes(`html/frukt_gront`) && !currentUrl.includes('html/frukt_gront_v2')) {
     display.displayProducts();
 }
-if (currentUrl.includes(`html/frukt_gront_v2`) ) {
+if (currentUrl.includes(`html/frukt_gront_v2`) || currentUrl.includes(`html/brodvarer`)) {
     display.displayProducts_v2();
 }
